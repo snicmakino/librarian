bff2ba86c66a605d551edf8966eeebc1b80b7308
snicmakino <mkn.fj.cd@gmail.com>
2017-09-11 04:00:21 +0900

    書籍取得処理でisbnを取得できるように変更

--- src/main/kotlin/jp/snicmakino/librarian/mapper/BookMapper.kt
+++ src/main/kotlin/jp/snicmakino/librarian/mapper/BookMapper.kt
@@ -1,10 +1,7 @@
 package jp.snicmakino.librarian.mapper
 
-import org.apache.ibatis.annotations.Insert
-import org.apache.ibatis.annotations.Mapper
-import org.apache.ibatis.annotations.Options
-import org.apache.ibatis.annotations.Select
 import jp.snicmakino.librarian.model.Book
+import org.apache.ibatis.annotations.*
 
 @Mapper
 interface BookMapper {
@@ -19,12 +16,25 @@ interface BookMapper {
 
     @Select("""
         SELECT
-            id, title, stock
+            id
+            ,title
+            ,stock
+            ,i10.isbn AS isbn10
+            ,i13.isbn AS isbn13
         FROM
-            book
+            book b
+            LEFT OUTER JOIN
+                isbn10 i10
+            ON  b.id = i10.book_id
+            LEFT OUTER JOIN
+                isbn13 i13
+            ON  b.id = i13.book_id
         WHERE
             id = #{id}
     """)
+    @Results(
+            Result(property = "isbn13.isbn", column = "isbn13"),
+            Result(property = "isbn10.isbn", column = "isbn10"))
     fun find(id: Int): Book
 
     @Insert("""
