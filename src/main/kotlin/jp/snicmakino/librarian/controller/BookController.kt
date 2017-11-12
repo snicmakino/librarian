849c65f28e858091602e2a4c3658e5aa819c0fc8
snicmakino <mkn.fj.cd@gmail.com>
2017-11-13 03:19:06 +0900

    書籍一覧画面に更新ボタンを追加

--- src/main/kotlin/jp/snicmakino/librarian/controller/BookController.kt
+++ src/main/kotlin/jp/snicmakino/librarian/controller/BookController.kt
@@ -3,9 +3,16 @@ package jp.snicmakino.librarian.controller
 import jp.snicmakino.librarian.mapper.BookMapper
 import jp.snicmakino.librarian.model.Book
 import org.springframework.stereotype.Controller
+import org.springframework.transaction.annotation.Transactional
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.*
 import javax.validation.Valid
+import org.springframework.beans.propertyeditors.CustomDateEditor
+import java.text.SimpleDateFormat
+import org.springframework.web.bind.WebDataBinder
+import org.springframework.web.bind.annotation.InitBinder
+
+
 
 @Controller
 @RequestMapping("/books")
@@ -35,6 +42,20 @@ class BookController(val bookMapper: BookMapper) {
         }
         if (book.isbn13.value != null) {
             bookMapper.registerIsbn13(book)
+        }
+        return "redirect:/books"
+    }
+
+    @PostMapping("update/{id}")
+    @Transactional
+    fun update(@PathVariable("id") id: Int, @ModelAttribute @Valid book: Book): String {
+        book.id = id
+        bookMapper.update(book)
+        if (book.isbn10.value != null) {
+            bookMapper.updateIsbn10(book)
+        }
+        if (book.isbn13.value != null) {
+            bookMapper.updateIsbn13(book)
         }
         return "redirect:/books"
     }
